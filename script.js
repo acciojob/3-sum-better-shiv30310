ed2295d850abbab88136544c3eba68b328ad3914
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-14 12:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
-// write your code here
+	
+    let res = []
+    for(let i=0; i<=arr.length-3; i++){
+        let temp = arr[i]
+        res.push(arr[i])
+        for(let j=i+1,t=0; t<2; t++){
+            temp += arr[j]
+            res.push(arr[j])
+            j++
+        }
+        if(temp <= target){
+            return res
+        }else{
+            res = []
+        }
+    }
   
 }
+
 
 module.exports = threeSum;
